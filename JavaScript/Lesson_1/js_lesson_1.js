7b987558d24fcc489a91fe924e66dfbd4d7e4752
Zapishnyi <zapishny@ukr.net>
2024-02-21 22:18:16 +0300

    JavaScript | Lesson 1 hw add

--- JavaScript/Lesson_1/js_lesson_1.js
+++ JavaScript/Lesson_1/js_lesson_1.js
@@ -1,5 +1,5 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
-//     Вивести кожну змінну за допомогою: console.log
+//     Вивести кожну змінну за допомогою: console.log;
 //
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 //
@@ -10,31 +10,31 @@
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
 console.log("-------------1--------------");
 let variable1 = 'hello';
-    console.log(variable1);
+console.log(variable1);
 let variable2 = 'owu';
-    console.log(variable2);
+console.log(variable2);
 let variable3 = 'com';
-    console.log(variable3);
+console.log(variable3);
 let variable4 = 'ua';
-    console.log(variable4);
+console.log(variable4);
 let variable5 = 1;
-    console.log(variable5);
+console.log(variable5);
 let variable6 = 10;
-    console.log(variable6);
+console.log(variable6);
 let variable7 = -999;
-    console.log(variable7);
+console.log(variable7);
 let variable8 = 123;
-    console.log(variable8);
+console.log(variable8);
 let variable9 = 3.14;
-    console.log(variable9);
+console.log(variable9);
 let variable10 = 2.7
-    console.log(variable10);
+console.log(variable10);
 let variable11 = 16;
-    console.log(variable11);
+console.log(variable11);
 let variable12 = true;
-    console.log(variable12);
+console.log(variable12);
 let variable13 = false;
-    console.log(variable13);
+console.log(variable13);
 console.log("-------------2--------------");
 let firstName = "Kostiantyn";
 let middleName = "Sergiyouvich";
@@ -45,9 +45,9 @@ console.log("-------------3--------------");
 let a = 100;
 let b = '100';
 let c = true;
-console.log(`variable a:${a} type:${typeof(a)}`)
-console.log(`variable b:${b} type:${typeof(b)}`)
-console.log(`variable c:${c} type:${typeof(c)}`)
+console.log(`variable a:${a} type:${typeof (a)}`)
+console.log(`variable b:${b} type:${typeof (b)}`)
+console.log(`variable c:${c} type:${typeof (c)}`)
 console.log("-------------4--------------");
 // let userName = prompt("Please enter your Name");
 // let userSurname = prompt("Please enter your Surname");
